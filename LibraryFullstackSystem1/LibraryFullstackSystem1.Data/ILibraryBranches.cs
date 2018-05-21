27c3e6db89af2a049c05e9a46d1dc3ff9701542b
Kelvin Cortez <kelvcortez@gmail.com>
2018-05-22 08:30:05 +1200

    LibraryBranchService and ILibraryBranches creation

---
+++ LibraryFullstackSystem1/LibraryFullstackSystem1.Data/ILibraryBranches.cs
@@ -0,0 +1,13 @@
+﻿using LibraryFullstackSystem1.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryFullstackSystem1.Data
+{
+    public interface ILibraryBranches
+    {
+        IEnumerable<LibraryBranch> GetAll();
+        LibraryBranch GetById(int id);
+    }
+}
---
+++ LibraryFullstackSystem1/LibraryFullstackSystem1.Services/LibraryBranchService.cs
@@ -0,0 +1,29 @@
+﻿using LibraryFullstackSystem1.Data;
+using LibraryFullstackSystem1.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LibraryFullstackSystem1.Services
+{
+    public class LibraryBranchService : ILibraryBranches
+    {
+        private readonly LibradyFullstackSystemDbContext _DbContext;
+
+        public LibraryBranchService(LibradyFullstackSystemDbContext DbContext)
+        {
+            _DbContext = DbContext;
+        }
+
+        public IEnumerable<LibraryBranch> GetAll()
+        {
+            return _DbContext.LibraryBranches;
+        }
+
+        public LibraryBranch GetById(int id)
+        {
+            return _DbContext.LibraryBranches.FirstOrDefault( p => p.Id == id);
+        }
+    }
+}
