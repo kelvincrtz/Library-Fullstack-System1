9012752c67447d17bee22200d356792dee887a85
Kelvin Cortez <kelvcortez@gmail.com>
2018-05-22 15:33:17 +1200

    Renaming variables at LibraryAssetService

--- LibraryFullstackSystem1/LibraryFullstackSystem1.Services/LibraryAssetService.cs
+++ LibraryFullstackSystem1/LibraryFullstackSystem1.Services/LibraryAssetService.cs
@@ -9,40 +9,40 @@ namespace LibraryFullstackSystem1.Services
 {
     public class LibraryAssetService : ILibraryAsset
     {
-        private readonly LibrarySystemDbContext _LibradyFullstackSystemDbContext;
+        private readonly LibrarySystemDbContext _DbContext;
 
-        public LibraryAssetService(LibrarySystemDbContext LibradyFullstackSystemDbContext)
+        public LibraryAssetService(LibrarySystemDbContext DbContext)
         {
-            _LibradyFullstackSystemDbContext = LibradyFullstackSystemDbContext;
+            _DbContext = DbContext;
         }
 
         public void Add(LibraryAsset newAsset)
         {
-            _LibradyFullstackSystemDbContext.Add(newAsset);
-            _LibradyFullstackSystemDbContext.SaveChanges();
+            _DbContext.Add(newAsset);
+            _DbContext.SaveChanges();
         }
 
         public IEnumerable<LibraryAsset> GetAll()
         {
-            return _LibradyFullstackSystemDbContext.LibraryAssets
+            return _DbContext.LibraryAssets
                 .Include(p => p.Status)
                 .Include(p => p.Location);
         }
 
         public string GetAuthorOrDirector(int id)
         {
-            var isBook = _LibradyFullstackSystemDbContext.LibraryAssets.OfType<Book>().Where(p => p.Id == id).Any();
-            var isVideo = _LibradyFullstackSystemDbContext.LibraryAssets.OfType<Video>().Where(p => p.Id == id).Any();
+            var isBook = _DbContext.LibraryAssets.OfType<Book>().Where(p => p.Id == id).Any();
+            var isVideo = _DbContext.LibraryAssets.OfType<Video>().Where(p => p.Id == id).Any();
 
             return isBook ?
-                _LibradyFullstackSystemDbContext.Books.FirstOrDefault(p => p.Id == id).Author :
-                _LibradyFullstackSystemDbContext.Videos.FirstOrDefault(p => p.Id == id).Director
+                _DbContext.Books.FirstOrDefault(p => p.Id == id).Author :
+                _DbContext.Videos.FirstOrDefault(p => p.Id == id).Director
                 ?? "Unknown";
         }
 
         public LibraryAsset GetById(int id)
         {
-            return _LibradyFullstackSystemDbContext.LibraryAssets
+            return _DbContext.LibraryAssets
                 .Include(p => p.Status)
                 .Include(p => p.Location)
                 .FirstOrDefault(p => p.Id == id);
@@ -50,37 +50,37 @@ namespace LibraryFullstackSystem1.Services
 
         public LibraryBranch GetCurrentLocation(int id)
         {
-            return _LibradyFullstackSystemDbContext.LibraryAssets
+            return _DbContext.LibraryAssets
                 .FirstOrDefault(i => i.Id == id).Location;
         }
 
         public string GetDeweyIndex(int id)
         {
-            if (_LibradyFullstackSystemDbContext.Books.Any(p => p.Id == id))
+            if (_DbContext.Books.Any(p => p.Id == id))
             {
-                return _LibradyFullstackSystemDbContext.Books.FirstOrDefault(p => p.Id == id).DeweyIndex;
+                return _DbContext.Books.FirstOrDefault(p => p.Id == id).DeweyIndex;
             }
             else return "";
         }
 
         public string GetIsbn(int id)
         {
-            if (_LibradyFullstackSystemDbContext.Books.Any(p => p.Id == id))
+            if (_DbContext.Books.Any(p => p.Id == id))
             {
-                return _LibradyFullstackSystemDbContext.Books.FirstOrDefault(p => p.Id == id).ISBN;
+                return _DbContext.Books.FirstOrDefault(p => p.Id == id).ISBN;
             }
             else return "";
         }
 
         public string GetTitle(int id)
         {
-            return _LibradyFullstackSystemDbContext.LibraryAssets.FirstOrDefault(p => p.Id == id).Title;
+            return _DbContext.LibraryAssets.FirstOrDefault(p => p.Id == id).Title;
         }
 
         public string GetType(int id)
         {
-            var isBook = _LibradyFullstackSystemDbContext.LibraryAssets.OfType<Book>().Where(p =>p.Id == id).Any();
-            var isVideo = _LibradyFullstackSystemDbContext.LibraryAssets.OfType<Video>().Where(p => p.Id == id).Any();
+            var isBook = _DbContext.LibraryAssets.OfType<Book>().Where(p =>p.Id == id).Any();
+            var isVideo = _DbContext.LibraryAssets.OfType<Video>().Where(p => p.Id == id).Any();
 
             return isBook ?
                 "Book" : "Video";
