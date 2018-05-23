0f48ad53f269ea64be04b218457685f3fdad8b56
Kelvin Cortez <kelvcortez@gmail.com>
2018-05-23 17:11:43 +1200

    Patron detail view update project

--- LibraryFullstackSystem1/LibraryFullstackSystem1/Models/Branch/BranchesIndexListingModel.cs
+++ LibraryFullstackSystem1/LibraryFullstackSystem1/Models/Branch/BranchesIndexListingModel.cs
@@ -12,5 +12,6 @@ namespace LibraryFullstackSystem1.Models.Branch
         public string ImageUrl { get; set; }
         public DateTime OpeningClosingHours { get; set; }
         public string Address { get; set; }
+        public string OpenDate { get; set; }
     }
 }
